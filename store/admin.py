076c3128fa474b1d16857d016e24ae7c9639d402
Hutty07 <opedayo80@gmail.com>
2022-03-23 17:40:26 +0000

    registered models on admin

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from . models import Genre, Book, Review
 
 # Register your models here.
+class BookAdmin(admin.ModelAdmin):
+    list_filter = ['is_available']
+    prepopulated_fields =  {'slug' : ('title',)}
+    
+
+admin.site.register(Genre)
+admin.site.register(Book, BookAdmin)
+admin.site.register(Review)
